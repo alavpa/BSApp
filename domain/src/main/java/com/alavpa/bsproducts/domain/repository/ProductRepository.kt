6f7dc59d0e6d5bf5641fa78d796178aac8d9ed43
Alex Avila <>
2020-08-26 19:03:53 +0200

    New: GetProducts interactor by page number and size.
    - Create GetProducts interactor test to get products by page number and size
    - Create Base Class Interactor
    - Create class GetProducts
    - Create ProductRepository interface
    - Create Product Mock Builder to provide mock

---
+++ domain/src/main/java/com/alavpa/bsproducts/domain/repository/ProductRepository.kt
@@ -0,0 +1,8 @@
+package com.alavpa.bsproducts.domain.repository
+
+import com.alavpa.bsproducts.domain.model.Product
+import io.reactivex.Single
+
+interface ProductRepository {
+    fun getProducts(page: Int, size: Int): Single<List<Product>>
+}
